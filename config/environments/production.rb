44691c07e081c7a35772e333e85f65ad61dad446
Ksenia Smith <smithkseniav@gmail.com>
2015-06-23 16:01:27 -0700

    not compressing assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,8 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   ## config.assets.js_compressor = :uglifier
-  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(mangle: false) }
+  ## config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(mangle: false) }
+  config.assets.compress = false
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
